aa086d3ac1571add84af09f588ac4ee4225deee0
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-09-04 09:57:43 +0300

    Inheritance test(1)

---
+++ oa_sale_order_inherit/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order İnheritance',
+    'version': '1.0',
+    'category': '',
+    'author': 'Onur Aksoy',
+    'sequence': -130,
+    'summary': ' Sale Order İnheritance Test ',
+    'description': """ Sale Order İnheritance Test """,
+    'depends': ['sale', 'project'],
+    'data':[
+        'so_inherit_view.xml',
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ oa_sale_order_inherit/models/so_inherit_model.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+class SaleOrderİnheritance(models.Model):
+    _name = "sale.order.inherit"
+    _inherit = ['project.project', 'project.task']
+    _description = "Sale Order İnheritance"
+
+    proje = fields.Many2one(comodel_name='project.project', ondelete='cascade')
+    task = fields.Many2one(comodel_name='project.task', ondelete='cascade')
