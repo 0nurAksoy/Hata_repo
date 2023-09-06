c6a08100d8900e703be034ee0a5c2b6b1c4f5991
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-09-06 10:07:35 +0300

    Basic inherit

--- oa_sale_order_inherit/models/so_inherit_model.py
+++ oa_sale_order_inherit/models/so_inherit_model.py
@@ -6,5 +6,7 @@ class SaleOrderİnheritance(models.Model):
     _inherit = ['project.project', 'project.task']
     _description = "Sale Order İnheritance"
 
-    proje = fields.Many2one(comodel_name='project.project', ondelete='cascade')
-    task = fields.Many2one(comodel_name='project.task', ondelete='cascade')
+    # proje = fields.Many2one(comodel_name='project.project', ondelete='cascade')
+    # task = fields.Many2one(comodel_name='project.task', ondelete='cascade')
+
+    test = fields.Many2one(string='name', ondelete='cascade')
