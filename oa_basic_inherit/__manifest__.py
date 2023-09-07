80bd0529aa23e2ced6b68cae3543124e465a9aa5
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-09-07 11:15:34 +0300

    Basic inheritance

---
+++ oa_basic_inherit/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Basic Sale Order Inheritance',
+    'version': '1.0',
+    'category': '',
+    'author': 'Onur Aksoy',
+    'sequence': -140,
+    'summary': ' Sale Order Inheritance Test ',
+    'description': """ Sale Order Inheritance Test """,
+    'depends': ['sale'],
+    'data':[
+        'basic_inherit_view.xml',
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
