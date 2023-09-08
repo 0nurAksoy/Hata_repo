b3b384b3baf6eb98a16d15fb00fd590b196ed2b9
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-09-08 14:23:51 +0300

    Related fields(1)

---
+++ oa_sale_order_inherit3/models/sale_order_inherit.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    project_choose_id = fields.Many2one(comodel_name="project.project", string="Proje")
+    task_choose_id = fields.Many2one(comodel_name="project.task", string="Görev")
