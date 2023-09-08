e4ef633fbbdddf44da160a6ae531308fca072330
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-09-08 12:52:33 +0300

    Test(2)
    Alan adları düzenlemeleri

--- oa_so_inherit/models/sale_order_inherit.py
+++ oa_so_inherit/models/sale_order_inherit.py
@@ -4,5 +4,5 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    project_id = fields.Many2one(comodel_name="project.project")
-    task_id = fields.Many2one(comodel_name="project.task")
+    project_choose_id = fields.Many2one(comodel_name="project.project")
+    task_choose_id = fields.Many2one(comodel_name="project.task")
