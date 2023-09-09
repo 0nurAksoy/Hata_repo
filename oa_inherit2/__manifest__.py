fb3ae50cf0a0465ed8bd161fd33dd2e6cc66f03b
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-09-09 16:32:42 +0300

    Test(3)

---
+++ oa_inherit2/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order Inheritance',
+    'version': '1.0',
+    'category': 'Sale',
+    'author': 'Onur Aksoy',
+    'summary': ' Sale Order Inheritance ',
+    'description': """ Sale Order Inheritance  """,
+    'depends': ['sale', 'project'],
+    'data':[
+        'views/sale_order_inherit_views.xml',
+    ],
+    'demo': [],
+}
